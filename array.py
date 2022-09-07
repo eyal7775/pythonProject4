5d66179a8674f8ed6c58f84eb95bddf35dcc8aa8
eyal7775 <eyal.naftalovich@mdclone.com>
2022-09-07 16:00:25 +0300

    fix version 3

--- array.py
+++ array.py
@@ -16,6 +16,23 @@ def get_second_higgest_number(numbers):
     else:
         raise Exception("the size list smaller from 2")
 
+def get_second_lowest_number(numbers):
+    n = len(numbers)
+    if n >= 2:
+        firstmin = min(numbers[0], numbers[1])
+        secondmin = max(numbers[0], numbers[1])
+        for i in range(2, n):
+            if numbers[i] < firstmin:
+                secondmin = firstmin
+                firstmin = numbers[i]
+            elif numbers[i] < secondmin and firstmin != numbers[i]:
+                secondmin = numbers[i]
+            elif firstmin == secondmin and secondmin != numbers[i]:
+                secondmin = numbers[i]
+        return secondmin
+    else:
+        raise Exception("the size list smaller from 2")
+
 def get_third_higgest_number(numbers):
     n = len(numbers)
     if n >= 3:
@@ -43,8 +60,39 @@ def get_third_higgest_number(numbers):
     else:
         raise Exception("the size list smaller from 3")
 
+def get_third_lowest_number(numbers):
+    n = len(numbers)
+    if n >= 3:
+        firstthree = numbers[:3]
+        firstmin = min(firstthree)
+        firstthree.remove(firstmin)
+        secondmin = min(firstthree)
+        thirdmin = max(firstthree)
+        del firstthree
+        for i in range(3, n):
+            if firstmin == secondmin and secondmin != numbers[i] and numbers[i] < thirdmin:
+                secondmin = numbers[i]
+            elif secondmin == thirdmin and thirdmin != numbers[i]:
+                thirdmin = numbers[i]
+            elif numbers[i] < firstmin:
+                thirdmin = secondmin
+                secondmin = firstmin
+                firstmin = numbers[i]
+            elif numbers[i] < secondmin and firstmin != numbers[i]:
+                thirdmin = secondmin
+                secondmin = numbers[i]
+            elif numbers[i] < thirdmin and secondmin != numbers[i]:
+                thirdmin = numbers[i]
+        return thirdmin
+    else:
+        raise Exception("the size list smaller from 3")
+
 numbers = [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7]
-h2 = get_second_higgest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 45
-print(h2)
-h3 = get_third_higgest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 37
-print(h3)
+secondmax = get_second_higgest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 45
+print(secondmax)
+secondmin = get_second_lowest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 2
+print(secondmin)
+thirdmax = get_third_higgest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 37
+print(thirdmax)
+thirdmin = get_third_lowest_number(numbers) # [47,47,2,6,13,7,1,19,26,37,12,4,13,6,5,45,13,7] => 4
+print(thirdmin)
